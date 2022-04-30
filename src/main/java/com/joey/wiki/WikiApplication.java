cf36dc211ca223e1041571eda05af4401f91c4d5
wangxc <chengx_996@163.com>
2022-04-30 17:34:34 +0800

    logback config

--- src/main/java/com/joey/wiki/WikiApplication.java
+++ src/main/java/com/joey/wiki/WikiApplication.java
@@ -1,13 +1,21 @@
 package com.joey.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        //SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("start success !!!");
+        LOG.info("address: \thttp://localhost:{}",env.getProperty("server.port"));
     }
 
 }
