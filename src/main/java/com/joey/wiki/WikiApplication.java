d3dde2a330ade60a72f6426ca2f41c4737e2dc03
wangxc <chengx_996@163.com>
2022-05-06 15:39:14 +0800

    开发/test/list接口，查询mysql表数据

--- src/main/java/com/joey/wiki/WikiApplication.java
+++ src/main/java/com/joey/wiki/WikiApplication.java
@@ -1,12 +1,15 @@
 package com.joey.wiki;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.joey")
+@MapperScan("com.joey.wiki.mapper")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
--- src/main/java/com/joey/wiki/controller/TestController.java
+++ src/main/java/com/joey/wiki/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.joey.wiki.controller;
 
+import com.joey.wiki.entity.TestEntity;
+import com.joey.wiki.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,6 +16,9 @@ import java.util.Map;
  */
 @RestController
 public class TestController {
+
+    @Resource
+    private TestService testService;
 
     //@RequestMapping("/hello")
     @GetMapping ("/hello")
@@ -24,4 +31,11 @@ public class TestController {
         //TODO 传参map问题未解决
         return "Hello World! Post: " + map.get("name");
     }
+
+    @GetMapping ("/test/list")
+    public List<TestEntity> list(){
+        return testService.list();
+    }
+
+
 }
